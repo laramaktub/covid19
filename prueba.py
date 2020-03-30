b6ef829cf8156c1c96999617a48be9562681660a
laramaktub <lara.cern@gmail.com>
2020-03-30 15:00:57 +0000

    prueba

---
+++ prueba.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_wtf import Form
+from wtforms import SelectField, TextField
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('db/covid19.db')
+c = conn.cursor()
+
+user="lari.lloret@googlemail.com"
+#x=c.execute("SELECT * FROM user_answers WHERE user = '%s' " % user).fetchall()
+y=c.execute("SELECT image FROM user_answers WHERE user ='%s' " % user).fetchall()
+print(y)
+#("SELECT edad, sexo,codigo, informe FROM images ORDER BY random() LIMIT 1;"
+x=c.execute("SELECT edad, sexo, codigo, informe FROM images  WHERE codigo NOT IN (SELECT image FROM user_answers WHERE user ='%s' % user) ORDER BY random()").fetchall()
+print(x)
+
+#x=c.execute("PRAGMA table_info(user_answers)").fetchall()
+#print(x)
+
+
+#x=c.execute("SELECT * FROM images").fetchall()
+#print(x)
+
